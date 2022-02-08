7955830eb80cb1b47b54fce125420e13169faef1
Akira Kawamata <autumn@piedey.co.jp>
2022-02-08 09:11:03 +0900

    debug read

--- MagML2HTML/MagML2HTML/Program.cs
+++ MagML2HTML/MagML2HTML/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MagMLParser;
+using System.Xml;
+
+const string filename = @"C:\Users\autumn.PDTOKYO4\OneDrive\SelfShare\0pub\C#\C#PRIMER2\org\20211107102022_▲_川俣晶の縁側_ソフトウェア_new%5FC#入門・全キーワード明快解説!.xmlcol";
+
+// DEBUG CODE
+XmlDocument doc = new XmlDocument();
+doc.Load( File.OpenRead(filename));
+
+
+
+var srcNode = doc.GetElementsByTagName("body",XmlNamespaces.Item);
+if (srcNode.Count > 0)
+{
+    var src = srcNode[0].InnerText;
+
+    var processor = new MagML();
+    processor.Compile(src, null, false);
+    Console.WriteLine(processor.RenderedBody);
+}
