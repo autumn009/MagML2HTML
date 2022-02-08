e7d004177bb1bc22369c9352df8555380fd16602
Akira Kawamata <autumn@piedey.co.jp>
2022-02-08 10:43:26 +0900

    Main enum files

--- MagML2HTML/MagML2HTML/Program.cs
+++ MagML2HTML/MagML2HTML/Program.cs
@@ -2,25 +2,31 @@
 using System.Diagnostics;
 using System.Xml;
 
-//const string filename = @"C:\Users\autumn.PDTOKYO4\OneDrive\SelfShare\0pub\C#\C#PRIMER2\org\20211107102022_▲_川俣晶の縁側_ソフトウェア_new%5FC#入門・全キーワード明快解説!.xmlcol";
-const string filename = @"C:\Users\autumn.PDTOKYO4\OneDrive\SelfShare\0pub\C#\C#PRIMER2\org\20211114105944_▲_川俣晶の縁側_ソフトウェア_new%5FC#入門・全キーワード明快解説!.xmlcol";
-
-const string tempout = @"c:\delme\delme.html";
-
-// DEBUG CODE
-XmlDocument doc = new XmlDocument();
-doc.Load( File.OpenRead(filename));
-
-
-
-var srcNode = doc.GetElementsByTagName("body",XmlNamespaces.Item);
-if (srcNode.Count > 0)
+if(Environment.GetCommandLineArgs().Length < 2)
 {
-    var src = srcNode[0].InnerText;
-
-    var processor = new MagML();
-    processor.Compile(src, null, false);
-    Console.WriteLine(processor.RenderedBody);
-    File.WriteAllText(tempout, processor.RenderedBody);
-    Process.Start("cmd.exe"," /c "+tempout);
+    Console.WriteLine("usage: MagML2HTML SourceFilesWithWildcard DstHTMLFile");
+    return;
 }
+
+var srcPaths = Environment.GetCommandLineArgs()[1];
+var dstFileName = Environment.GetCommandLineArgs()[2];
+
+using (var dstFile = File.CreateText(dstFileName))
+{
+    foreach (var filename in Directory.EnumerateFiles(Path.GetDirectoryName(srcPaths), Path.GetFileName(srcPaths)))
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(File.OpenRead(filename));
+        var srcNode = doc.GetElementsByTagName("body", XmlNamespaces.Item);
+        if (srcNode.Count > 0)
+        {
+            var src = srcNode[0].InnerText;
+
+            var processor = new MagML();
+            processor.Compile(src, null, false);
+            //Console.WriteLine(processor.RenderedBody);
+            dstFile.WriteLine(processor.RenderedBody);
+        }
+    }
+}
+Process.Start("cmd.exe", " /c " + dstFileName);
